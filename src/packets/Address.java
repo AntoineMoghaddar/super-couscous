8462921479ccc7406829c6d23cc55f227d2f2f8d
Antoine Moghaddar <a.a.moghaddar@student.utwente.nl>
2021-04-09 15:57:23 +0200

    Cost to node added to Address Object | Antoine Moghaddar

--- src/packets/Address.java
+++ src/packets/Address.java
@@ -4,11 +4,13 @@ import helperClasses.logging.Logger;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 public class Address {
 
     private final int ip_id;
     private InetAddress ip_address;
+    private HashMap<Address, Integer> cost_to_node;
 
     public Address(String ipaddress, int id) {
         this.ip_address = converter(ipaddress);
@@ -43,4 +45,18 @@ public class Address {
     public InetAddress getIp_address() {
         return ip_address;
     }
+
+    public void addCostOfNode(Address address, Integer RTT_time) {
+        cost_to_node.put(address, RTT_time);
+    }
+
+    public HashMap<Address, Integer> getCost_to_node() {
+        return cost_to_node;
+    }
+
+//    public Integer getCostSpecificNode(){
+//        for (Address a :
+//                cost_to_node) {
+//        }
+//    }
 }
