819715ad0ee80eb4d248c56ee8cf47a06b36790a
ujjwaldodeja <75213802+ujjwaldodeja@users.noreply.github.com>
2021-04-10 12:47:16 +0200

    small changes in messaGE

--- src/framework/client/Message.java
+++ src/framework/client/Message.java
@@ -2,21 +2,22 @@ package framework.client;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
+import java.nio.ReadOnlyBufferException;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 import packets.Address;
 import model.CouscousModel;
 
-//import protocol.packets.Packet;
+    // DON'T MAKE ANY CHANGES TO ANYTHING--- Ujjwal
 public class Message {
     private MessageType type;
     private ByteBuffer data;
-    private ArrayList<Integer> destination_nodes;
-    private int source_id;
-    //  private Packet pack;
-
-    public int get_source_add() {
+   // private ArrayList<Integer> destination_nodes; // Activate after dry run
+   // private int source_id;                        // Activate after dry run
+    //private int destination_id;
+   /* public int get_source_add() {
 
         ArrayList<Address> all_nodes;
         all_nodes = CouscousModel.getInstance().getAddresses();
@@ -32,39 +33,52 @@ public class Message {
             }
         }
         return source_id;
-    }
+    }*/ // Activate after dry run
 
+    // public int get_dest_add() { return destination_id;}
     public Message(MessageType type) {
         this.type = type;
     }
-    /*Notes for Antoine
-     *  if you get done with the ID/IP
-     */
 
     public Message(MessageType type, ByteBuffer data) {
         this.type = type;
-        //ByteBuffer bb1 = ByteBuffer.allocate(32);
-        //int source = get_source_add();
-        // byte[] dest_address = getIp_id.getBytes();
-        //bb1.put((byte) source);
-        //bb1.put(dest_address);
-        //bb1.put(data);
-        this.data = data;
+        try {
+            byte[] gg = new byte[data.position()];
+            data.flip();
+            data.get(gg, 0 , gg.length);
+            data.clear();
+            data.put((byte) 68);
+//          bb1.put((byte)destination);
+            data.put(gg);
+            this.data = data;
+        }catch(BufferOverflowException e) {
+            System.out.print("current position is higher than limit");
+            System.out.print("Exception e");
+        }catch(ReadOnlyBufferException e) {
+            System.out.print("Exception again" + e);
+        }
     }
 
     public Message(MessageType type, ByteBuffer data, int data_length) {
         this.type = type;
-
-        //ByteBuffer bb1 = ByteBuffer.allocate(32);
-        //int source = get_source_add();
-        //byte[] dest_address = getIp_id.getBytes();
-        //bb1.put((byte) source);
-//        //  bb1.put(dest_address);
-        //bb1.put((byte) data_length);
+        try {
+            byte[] gg = new byte[data.position()];
+            data.flip();
+            data.get(gg, 0 , gg.length);
+            data.clear();
+            data.put((byte) 68);
+//          bb1.put((byte)destination);
+            data.put((byte) data_length);
 //// int header_length = source.size() + dest.size() + data_length.size();
-        //bb1.put((byte) header_length);
-       // bb1.put(data);
-        this.data = data;
+            //bb1.put((byte) header_length);
+            data.put(gg);
+            this.data = data;
+        } catch (BufferOverflowException e) {
+            System.out.print("current position is higher than limit");
+            System.out.print("Exception e");
+        } catch (ReadOnlyBufferException e) {
+            System.out.print("Exception again" + e);
+        }
     }
 
     public MessageType getType() {
@@ -74,4 +88,18 @@ public class Message {
     public ByteBuffer getData() {
         return data;
     }
+   /* public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String give = "";
+        give = sc.nextLine();
+        ByteBuffer bb = ByteBuffer.allocate(32);
+        bb.put(give.getBytes(), 0, give.length());
+        Message mm = new Message(MessageType.DATA, bb, give.length());
+        bb.flip();
+        try{
+        System.out.print("fuck" + " "+ (char) bb.get(21));
+        }catch(IndexOutOfBoundsException e){
+            System.out.print("The exception fucked it over");
+        }
+    } */
 }
