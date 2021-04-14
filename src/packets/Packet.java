c9875f7add0d360d2d40cc2e284fab27eeab0532
The Ultimate Moose <antoine_moghaddar@hotmail.com>
2021-04-14 11:17:42 +0200

    Beginning of new Eraaaa | Antoine Moghaddar

--- src/packets/Packet.java
+++ src/packets/Packet.java
@@ -3,7 +3,7 @@ package packets;
 public class Packet {
     private static final int SIZE = 32;
 
-    private int s_address, d_addres, seq, offset;
+    private int s_address, d_address, seq, offset;
     private boolean followup;
 
     private String data;
@@ -18,9 +18,70 @@ public class Packet {
         this.data = data;
     }
 
-    public Packet(int[] packet){
+    public Packet(int[] packet) {
         this.s_address = packet[0];
+        this.d_address = packet[1];
+        this.seq = packet[2];
+        this.offset = packet[3];
+        this.followup = (packet[4] == 0);
+        //string to bytes
+//        byte[] inputBytes = data.getBytes(); // get bytes from input
+//        ByteBuffer toSend = ByteBuffer.allocate(inputBytes.length); // make a new byte buffer with the length of the input string
+//        toSend.put(inputBytes, 0, inputBytes.length); // copy the input string into the byte buffer.
+//        Message msg;
+//        this.data = packet[5];
 
     }
 
+    public static int getSIZE() {
+        return SIZE;
+    }
+
+    public int getS_address() {
+        return s_address;
+    }
+
+    public void setS_address(int s_address) {
+        this.s_address = s_address;
+    }
+
+    public int getD_address() {
+        return d_address;
+    }
+
+    public void setD_address(int d_address) {
+        this.d_address = d_address;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public boolean hasFollowUp() {
+        return followup;
+    }
+
+    public void setFollowup(boolean followup) {
+        this.followup = followup;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
